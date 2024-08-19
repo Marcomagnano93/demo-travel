438d8ef2b0ff558331dff5a033a2098532fe57f7
Marcomagnano93 <marcog.magnano@gmail.com>
2024-08-19 12:00:15 +0200

    Start db connection

--- index.php
+++ index.php
@@ -4,3 +4,23 @@ require 'functions.php';
 require 'route.php';
 
  routeToController($uri, $routes);
+
+
+ //connect to server
+// $dsn = "mysql:host=localhost;port=8000;dbname=travels";
+// new PDO($dsn);
+
+
+//classe
+//  class Travel {
+//     public $name;
+//     public $days;
+//     public $description;
+//     public $rating;
+
+//*****  methods (function di una classe)  *****
+// public function example(){
+
+// }
+//  }
+
--- view/index.view.php
+++ view/index.view.php
@@ -1,5 +1,6 @@
 <?php require 'partials/head.php'
 
+
     
 ?>
 <body>
@@ -7,7 +8,17 @@
 <?php require 'partials/nav.php' ?>
     </header>
     <main>
+        <?php 
+        // $db = connection();
 
+        // $sql = "SELECT * FROM `travels`";
+        // $result = $db->query($sql);
+
+        // foreach($result as $item){
+        //     echo $item['name'];
+        // };
+        
+        ?>
     </main>
     <footer>
 <?php require 'partials/footer.php' ?>
